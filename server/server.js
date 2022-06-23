d724878d89a5ce983603985ce9752408c20638f3
masterkai <kai0919@hotmail.com>
2022-06-23 14:08:19 +0800

    set client & server basics

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/api/signup', (req, res) => {
+    res.json({
+        data: 'you hit signup endpoint!'
+    })
+})
+
+const port = process.env.port || 8000
+
+app.listen(port, () => {
+    console.log(`Api is running on port${port}`)
+})
