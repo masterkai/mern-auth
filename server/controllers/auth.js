e5ae5ec94899af84fb27aaa95c305d468e3dd06a
masterkai <kai0919@hotmail.com>
2022-06-26 19:35:19 +0800

    Signin user

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -102,3 +102,31 @@ exports.accountActivation = (req, res) => {
     });
   }
 };
+
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  // check if user exist
+  User.findOne({ email }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User with that email does not exist. Please signup",
+      });
+    }
+    // authenticate
+    if (!user.authenticate(password)) {
+      return res.status(400).json({
+        error: "Email and password do not match",
+      });
+    }
+    // generate a token and send to client
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    const { _id, name, email, role } = user;
+
+    return res.json({
+      token,
+      user: { _id, name, email, role },
+    });
+  });
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 // import controllers
-const { signup, accountActivation } = require("../controllers/auth");
+const { signup, accountActivation, signin } = require("../controllers/auth");
 
 // import validators
-const { userSignupValidator } = require("../validators/auth");
+const {
+  userSignupValidator,
+  userSigninValidator,
+} = require("../validators/auth");
 const { runValidation } = require("../validators");
 
 router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/account-activation", accountActivation);
+router.post("/signin", userSigninValidator, runValidation, signin);
 
 module.exports = router;
