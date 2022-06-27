9ae59a4635bb3054b29255fa273ff6c2fa9782a3
masterkai <kai0919@hotmail.com>
2022-06-27 17:53:43 +0800

    User signin

--- client/src/core/Layout.tsx
+++ client/src/core/Layout.tsx
@@ -31,6 +31,11 @@ function Layout({ children }: LayoutProps) {
         </Link>
       </li>
       <li className="nav-item">
+        <Link to="/signin" className="text-light nav-link">
+          Signin
+        </Link>
+      </li>
+      <li className="nav-item">
         <Link to="/signup" className="text-light nav-link">
           Signup
         </Link>
