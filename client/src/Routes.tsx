52df8d161a1a770d65877ca7bb8525322f553f9b
masterkai <kai0919@hotmail.com>
2022-06-28 02:00:28 +0800

    client: User signin

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import App from "./App";
 import Signup from "./auth/Signup";
 import Signin from "./auth/Signin";
-import Activate from "./auth/Activate";
 
 const Routes = () => {
   return (
@@ -12,7 +11,6 @@ const Routes = () => {
         <Route path="/" exact component={App} />
         <Route path="/signup" exact component={Signup} />
         <Route path="/signin" exact component={Signin} />
-        <Route path="/auth/activate/:token" exact component={Activate} />
       </Switch>
     </BrowserRouter>
   );
